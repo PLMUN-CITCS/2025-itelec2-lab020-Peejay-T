ed6bc11d80009ea8350ab55ee467e91d5f9c7b19
Peejay-T <pjturing_bsit@plmun.edu.ph>
2025-03-13 12:26:35 +0800

    Update even_odd_checker_functions.py

--- even_odd_checker_functions.py
+++ even_odd_checker_functions.py
@@ -2,8 +2,6 @@ def get_integer_input():
     while True:
         try:
             return int(input("Enter an integer: "))
-        except ValueError:
-            print("Invalid input. Please enter a valid integer.")
 
 def check_even_odd(number):
     if number % 2 == 0:
