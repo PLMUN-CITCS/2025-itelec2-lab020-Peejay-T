23e57846fb7c5092641eb5cf369d4ae6f1a4e6d0
Peejay-T <pjturing_bsit@plmun.edu.ph>
2025-03-13 12:33:56 +0800

    Update even_odd_checker_functions.py

--- even_odd_checker_functions.py
+++ even_odd_checker_functions.py
@@ -4,13 +4,11 @@ def get_integer_input():
             return int(input("Enter an integer: "))
         except ValueError:
             print("Invalid input. Please input an integer)
-
 def check_even_odd(number):
     if number % 2 == 0:
         return f"{number} is an Even number."
     else:
         return f"{number} is an Odd number."
-
 num = get_integer_input() 
 result = check_even_odd(num)
 print(result)
